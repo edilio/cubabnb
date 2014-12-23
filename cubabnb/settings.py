f540d4a0f974be28e9bbf3edd47879ac6ef18a4c
Edilio Gallardo <edilio73@gmail.com>
2014-12-23 17:04:39 -0500

    getting rid of sqlite database

--- cubabnb/settings.py
+++ cubabnb/settings.py
@@ -28,23 +28,13 @@ ADMINS = (
 MANAGERS = ADMINS
 
 DATABASES = {
-    'default_server': {
+    'default': {
         'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': os.environ.get('NAME', 'cubabb'),                      # Or path to database file if using sqlite3.
-        # The following settings are not used with sqlite3:
         'USER': os.environ.get('USER_NAME'),
         'PASSWORD': os.environ.get('PASSWORD'),
         'HOST': os.environ.get('HOST'),                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
         'PORT': os.environ.get('PORT', '3306'),                      # Set to empty string for default.
-    },
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': os.path.join(BASE_DIR, 'cubabnb.sql'),                      # Or path to database file if using sqlite3.
-        # The following settings are not used with sqlite3:
-        'USER': '',
-        'PASSWORD': '',
-        'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
-        'PORT': '',                      # Set to empty string for default.
     }
 }
 
