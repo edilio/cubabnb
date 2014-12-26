ae5df50ca9e90c15f437a8c10bb2f3a8e7259d7f
Edilio Gallardo <edilio73@gmail.com>
2014-12-26 16:38:47 -0500

    use rooms instead of properties for urls

--- apps/bb/urls/html.py
+++ apps/bb/urls/html.py
@@ -3,6 +3,6 @@ from ..views import html
 from django.conf.urls import patterns, url
 
 urlpatterns = patterns('bb.views',
-    url(r'^properties$', html.PropertyPageView.as_view(), name='property_list'),
-    url(r'^properties/(?P<slug>[-\w\d]+)/$', html.PropertyDetail.as_view(), name='property_detail'),
+    url(r'^rooms$', html.PropertyPageView.as_view(), name='property_list'),
+    url(r'^rooms/(?P<slug>[-\w\d]+)/$', html.PropertyDetail.as_view(), name='property_detail'),
 )
